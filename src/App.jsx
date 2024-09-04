22d4805da55368a48ddba1e67bfcf18693b144fc
lucasdias0812 <99883219+lucasdias0812@users.noreply.github.com>
2024-09-03 21:26:12 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './Header'
+
+function App() {
+
+/*const style = {
+  backgroundColor: "blue",
+  color: "white"
+}*/
+  
+
+  return(
+    <>
+    <Header/>
+    <h1 className='text-blue-800 hover:text-6x1'>Pagina principal</h1>
+    </>
+  )
+}
+
+export default App
